64813bbf5421293357d7176eadbfd948b25ebf92
kushmohit <102211179+kushmohit@users.noreply.github.com>
2022-05-08 16:25:42 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { AppserService } from './appser.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-Task';
-  buttonvalue="login"
-  
+  buttonvalue="login";
+  username="admin";
+
+  constructor(private serv:AppserService){
+
+  }
+
+  ngOnInit(): void {
+
+
+this.serv.title.subscribe((res)=>{
+this.username=res
+})
+this.serv.login.subscribe((res)=>{
+  this.buttonvalue=res
+  })
+  this.serv.setvalue();
+  }
+
+
 }
