a5ac7290664967feae7c2828ec38d50b98d60fd9
kushmohit <102211179+kushmohit@users.noreply.github.com>
2022-05-08 16:27:11 +0530

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup,FormControl,Validators } from '@angular/forms';
 import {  Router } from '@angular/router';
+import { AppserService } from '../appser.service';
 
 @Component({
   selector: 'app-about',
@@ -10,7 +11,7 @@ import {  Router } from '@angular/router';
 export class AboutComponent implements OnInit {
   loginpage: FormGroup;
   error: boolean;
-  constructor(private router: Router,
+  constructor(private router: Router,private serv:AppserService,
     private fb:FormBuilder) { }
 
   ngOnInit(): void {
@@ -23,11 +24,13 @@ export class AboutComponent implements OnInit {
 
     if(this.loginpage.value.username=='admin' && this.loginpage.value.password=='admin'){
       this.router.navigate(['work']);
+      this.serv.title.emit(this.loginpage.value.username);
+      this.serv.login.emit("logout");
     }
     else{
       this.error=true
     }
-    
+
 
   }
 }
