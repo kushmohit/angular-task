748be0422aa660aa7f1ecb5b79caaed3b0c42126
kushmohit <102211179+kushmohit@users.noreply.github.com>
2022-05-08 16:25:05 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path:'' ,component:HomeComponent ,pathMatch:'full'},
+  {path:''  ,redirectTo: '/about',pathMatch:'full'},
+  {path:'home'  ,component:HomeComponent},
   { path: 'about', loadChildren: () => import('./about/about-module.module').then(m => m.AboutModuleModule) },
   { path: 'work', loadChildren: () => import('./work/work-module.module').then(m => m.WorkModuleModule) }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
